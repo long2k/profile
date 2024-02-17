1ee2efa0f3e6a0fb473f4c791875ced1a73f3af6
ken full-stack <ken@codesource.com.au>
2024-02-17 18:31:38 +0700

    fix: revise nextjs.yml

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./public/**/**/*.{svg}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
